f52893ee0704f9525dea0ca94d91d6013e9e6d0b
Jimmy Dansbo <jimmy@dansbo.dk>
2021-02-02 22:24:55 +0100

    Different library function being tested.

--- vtuitest.asm
+++ vtuitest.asm
@@ -1,27 +1,47 @@
 *=$0801
 !byte $0C,$08,$0A,$00,$9E,' ','2','0','6','4',$00,$00,$00
 *=$0810
+x16		= $22
+x16l		= x16
+x16h		= x16+1
+x17		= $24
+x17l		= x17
+x17h		= x17+1
+x18		= $26
+x18l		= x18
+x18h		= x18+1
+x19		= $28
+x19l		= x19
+x19h		= x19+1
 
 LIBSTART=$0400
 
 VTUI_initialize	= LIBSTART
-VTUI_screen_set	= LIBSTART+2
+VTUI_screen_set	= VTUI_initialize+2
 VTUI_clear	= VTUI_screen_set+3
 VTUI_set_stride	= VTUI_clear+3
 VTUI_set_decr	= VTUI_set_stride+3
-VTUI_hline	= VTUI_set_decr+3
+VTUI_plot_char	= VTUI_set_decr+3
+VTUI_scan_char	= VTUI_plot_char+3
+VTUI_hline	= VTUI_scan_char+3
 VTUI_vline	= VTUI_hline+3
 VTUI_gotoxy	= VTUI_vline+3
 VTUI_print_str	= VTUI_gotoxy+3
 VTUI_fill_box	= VTUI_print_str+3
+VTUI_pet2scr	= VTUI_fill_box+3
+VTUI_scr2pet	= VTUI_pet2scr+3
 
 main:
 	jsr	load_library	; Load the library
 
 	jsr	VTUI_initialize	; Initialize jumptable in library
+!byte $db
+	ldy	#3
+	lda	#11
+	jsr	VTUI_gotoxy
+	jsr	VTUI_scan_char
+	jsr	VTUI_scr2pet
 
-	lda	#$10		; White background / black text
-	jsr	VTUI_clear	; Clear screen
 	rts
 
 load_library:
@@ -41,3 +61,4 @@ load_library:
 
 Fname	!text	"VTUI.BIN"
 End_fname
+	!byte	0
