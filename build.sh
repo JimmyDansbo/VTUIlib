edabf046f3cf8d079a1da26c7810835f7435f8f2
Jimmy Dansbo <jimmy@dansbo.dk>
2021-02-03 11:21:13 +0100

    Added symbol output to build script and gitignore list

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf *.PRG *.BIN
-acme -f cbm -o VTUI.BIN vtuilib-generic.inc
-acme -f cbm -o VTUITEST.PRG vtuitest.asm
+acme -f cbm -o VTUI.BIN -l vtui.sym vtuilib-generic.inc
+acme -f cbm -o VTUITEST.PRG -l vtuitest.sym vtuitest.asm
 
