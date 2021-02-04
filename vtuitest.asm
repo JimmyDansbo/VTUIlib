953db03ce96604f0c5722a8ac4cc3f005e3949fb
Jimmy Dansbo <jimmy@dansbo.dk>
2021-02-04 07:00:27 +0100

    Testing borders

--- vtuitest.asm
+++ vtuitest.asm
@@ -1,18 +1,7 @@
 *=$0801
 !byte $0C,$08,$0A,$00,$9E,' ','2','0','6','4',$00,$00,$00
 *=$0810
-x16		= $22
-x16l		= x16
-x16h		= x16+1
-x17		= $24
-x17l		= x17
-x17h		= x17+1
-x18		= $26
-x18l		= x18
-x18h		= x18+1
-x19		= $28
-x19l		= x19
-x19h		= x19+1
+!src	"vtuilib-acme.inc"
 
 ;LIBSTART=$0400
 LIBSTART=PRELIB+2
@@ -22,12 +11,12 @@ VTUI_screen_set	= VTUI_initialize+2
 VTUI_clear	= VTUI_screen_set+3
 VTUI_set_stride	= VTUI_clear+3
 VTUI_set_decr	= VTUI_set_stride+3
-VTUI_plot_char	= VTUI_set_decr+3
+VTUI_gotoxy	= VTUI_set_decr+3
+VTUI_plot_char	= VTUI_gotoxy+3
 VTUI_scan_char	= VTUI_plot_char+3
 VTUI_hline	= VTUI_scan_char+3
 VTUI_vline	= VTUI_hline+3
-VTUI_gotoxy	= VTUI_vline+3
-VTUI_print_str	= VTUI_gotoxy+3
+VTUI_print_str	= VTUI_vline+3
 VTUI_fill_box	= VTUI_print_str+3
 VTUI_pet2scr	= VTUI_fill_box+3
 VTUI_scr2pet	= VTUI_pet2scr+3
@@ -41,30 +30,12 @@ main:
 	lda	#$10
 	jsr	VTUI_clear
 
-	ldy	#3
-	lda	#5
-	jsr	VTUI_gotoxy
-
-	ldx	#$10
-	lda	#$4F
-	jsr	VTUI_plot_char
-
-	lda	#$77
-	sta	x16l
-	ldy	#10
-	jsr	VTUI_hline
-
-	lda	#$50
-	jsr	VTUI_plot_char
-
-!byte $db
 	ldy	#10
 	lda	#10
 	jsr	VTUI_gotoxy
 
-	lda	#5
+	lda	#4
 	sta	x17l
-	lda	#5
 	sta	x17h
 	ldx	#$12
 	lda	#5
