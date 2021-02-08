4b1be2a4f771e856a68f730a34a9faa7af3532b8
Jimmy Dansbo <jimmy@dansbo.dk>
2021-02-08 15:54:32 +0100

    Renamed vtuilib-generic.inc to vtuilib-generic.asm as it is not an include file.

--- VTUIlib-generic/build.sh
+++ VTUIlib-generic/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf *.PRG *.BIN
-acme -f cbm -o VTUI0.1.BIN vtuilib-generic.inc
+acme -f cbm -o VTUI0.1.BIN vtuilib-generic.asm
 acme -f cbm -o EXAMPL01.PRG example01.asm
 acme -f cbm -o EXAMPL02.PRG example02.asm
 
